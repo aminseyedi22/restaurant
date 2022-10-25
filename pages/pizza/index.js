3610274171d60b1609994aa4ed2bd0fef2af7968
Amin Seyedi <39224500+aminseyedi22@users.noreply.github.com>
2022-10-25 19:56:24 +0330

    working on pizza page 2

--- pages/pizza/index.js
+++ pages/pizza/index.js
@@ -1,7 +1,28 @@
-import styles from '../../styles/Pizza.module.css'
+import styles from "../../styles/Pizza.module.css";
 
 export default function Pizza() {
     return (
-        <div>hi</div>
-    )
+        <div>
+            <div className={styles.title}>پیتزا ها</div>
+            <div className={`${styles.menutext} row`}>
+                <div className='cols'>
+                    <div className={`${styles.item} row`}>
+                        <span className="col-auto">پپرونی کوچک</span>
+                        <span className={`${styles.dash} col`}></span>
+                        <span className="col-auto">150000 تومان</span>
+                        <span className={`${styles.ord} col-2`}> - <span>0</span> عدد </span>
+                    </div>
+                    <div className={`${styles.item} row`}>
+                        <span className="col-auto">پپرونی کوچک</span>
+                        <span className={`${styles.dash} col`}></span>
+                        <span className="col-auto">150000 تومان</span>
+                        <span className={`${styles.ord} col-2`}> - <span>0</span> عدد </span>
+                    </div>
+
+
+                </div>
+
+            </div>
+        </div>
+    );
 }
