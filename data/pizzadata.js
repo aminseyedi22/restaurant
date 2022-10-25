7122cf1c70df91fa5911cdf023a561f224704840
Amin Seyedi <39224500+aminseyedi22@users.noreply.github.com>
2022-10-25 20:58:57 +0330

    changing pizza page 3

---
+++ data/pizzadata.js
@@ -0,0 +1,37 @@
+export const pizza = [
+  {
+    name: "پپرونی",
+    price: 150000,
+    order: 0,
+  },
+  {
+    name: "مخلوط",
+    price: 130000,
+    order: 0,
+  },
+  {
+    name: "رست بیف",
+    price: 170000,
+    order: 0,
+  },
+  {
+    name: "گوشت",
+    price: 160000,
+    order: 0,
+  },
+  {
+    name: "مرغ",
+    price: 150000,
+    order: 0,
+  },
+  {
+    name: " قارچ و پنیر",
+    price: 150000,
+    order: 0,
+  },
+  {
+    name: "سبزیجات",
+    price: 150000,
+    order: 0,
+  },
+];
--- pages/pizza/index.js
+++ pages/pizza/index.js
@@ -1,28 +1,46 @@
 import styles from "../../styles/Pizza.module.css";
+import { pizza } from "../../data/pizzadata";
+import { useState } from "react";
 
-export default function Pizza() {
-    return (
-        <div>
-            <div className={styles.title}>پیتزا ها</div>
-            <div className={`${styles.menutext} row`}>
-                <div className='cols'>
-                    <div className={`${styles.item} row`}>
-                        <span className="col-auto">پپرونی کوچک</span>
-                        <span className={`${styles.dash} col`}></span>
-                        <span className="col-auto">150000 تومان</span>
-                        <span className={`${styles.ord} col-2`}> - <span>0</span> عدد </span>
-                    </div>
-                    <div className={`${styles.item} row`}>
-                        <span className="col-auto">پپرونی کوچک</span>
-                        <span className={`${styles.dash} col`}></span>
-                        <span className="col-auto">150000 تومان</span>
-                        <span className={`${styles.ord} col-2`}> - <span>0</span> عدد </span>
-                    </div>
+export default function Pizza({ data }) {
+  const [reload, setReload] = useState([]);
+  function refresh() {
+    setReload([]);
+  }
 
-
-                </div>
-
+  return (
+    <div>
+      <img src='/images/arrowdown.png' alt='' className={styles.arrow} />
+      <div className={styles.title}>پیتزا ها</div>
+      <div className={`${styles.menutext} row`}>
+        <div className='cols'>
+          {data.map((val) => (
+            <div
+              className={`${styles.item} ${styles.preventselect} row`}
+              onClick={() => {
+                val.order++;
+                refresh();
+              }}
+            >
+              <span className='col-auto'>{val.name}</span>
+              <span className={`${styles.dash} col`}></span>
+              <span className='col-auto'>{val.price} تومان</span>
+              <span className={`${styles.ord} col-2`}>
+                {" "}
+                <span>{val.order}</span> عدد{" "}
+              </span>
             </div>
+          ))}
         </div>
-    );
+      </div>
+    </div>
+  );
 }
+
+export const getStaticProps = async (ctx) => {
+  return {
+    props: {
+      data: pizza,
+    },
+  };
+};
