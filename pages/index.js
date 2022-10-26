66df6ceb6237f7ebf4faa43d3760b8d20784a6bc
Amin Seyedi <39224500+aminseyedi22@users.noreply.github.com>
2022-10-26 22:01:24 +0330

    working on pizza page 4

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,32 @@
-import Link from 'next/link'
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Link href='/pizza'><img src='/images/pizza.png' width={320} height={330} className={styles.menu}></img></Link>
-      <img src='/images/drinks.png' width={330} height={300} className={styles.menu}></img>
-      <img src='/images/burger.png' width={330} height={300} className={styles.menu}></img>
-    </div >
-  )
+      <div className={`${styles.tit}`}>
+        <img src="/images/logo.png" alt="" />
+      </div>
+      <Link href='/pizza'>
+        <img
+          src='/images/pizza.png'
+          width={320}
+          height={330}
+          className={styles.menu}
+        ></img>
+      </Link>
+      <img
+        src='/images/drinks.png'
+        width={330}
+        height={300}
+        className={styles.menu}
+      ></img>
+      <img
+        src='/images/burger.png'
+        width={330}
+        height={300}
+        className={styles.menu}
+      ></img>
+    </div>
+  );
 }
--- pages/pizza/index.js
+++ pages/pizza/index.js
@@ -7,33 +7,66 @@ export default function Pizza({ data }) {
   function refresh() {
     setReload([]);
   }
+  function increase(value) {
+    value.order++;
+    refresh();
+  }
+  function decrease(value) {
+    value.order--;
+    refresh();
+  }
 
   return (
     <div>
-      <img src='/images/arrowdown.png' alt='' className={styles.arrow} />
-      <div className={styles.title}>پیتزا ها</div>
+
+      <img
+        src='/images/arrowdown.png'
+        alt=''
+        className={`${styles.arrow} d-none d-lg-block`}
+      />
+      <div className={`${styles.title}`}><img src="/images/pizzaWord.png" width={`250px`} alt="" /></div>
       <div className={`${styles.menutext} row`}>
-        <div className='cols'>
+        <div className='cols '>
           {data.map((val) => (
             <div
-              className={`${styles.item} ${styles.preventselect} row`}
-              onClick={() => {
-                val.order++;
-                refresh();
-              }}
-            >
-              <span className='col-auto'>{val.name}</span>
-              <span className={`${styles.dash} col`}></span>
-              <span className='col-auto'>{val.price} تومان</span>
-              <span className={`${styles.ord} col-2`}>
-                {" "}
-                <span>{val.order}</span> عدد{" "}
+              className={`${styles.item} ${styles.preventselect} row  `} >
+              <span className='col-lg-auto col-auto'>{val.name}</span>
+              <span className={`${styles.dash} col-lg col`}></span>
+              <span className='col-lg-auto  cols text-center'>
+                {val.price} تومان
+              </span>
+              <span className="col-3">
+                <div className="row">
+                  <span
+                    className=' col-lg-auto  col-auto mb-4 mb-lg-0 me-3'
+                    onClick={() => {
+                      increase(val);
+                    }}
+                  >
+                    +
+                  </span>
+                  <span
+                    className={`${styles.ord} col-lg-auto  col-auto ms-auto me-auto `}
+                  >
+                    {" "}
+                    <span>{val.order}</span> عدد{" "}
+                  </span>
+                  <span
+                    className=' col-lg-auto  col-auto mb-4 mb-lg-0'
+                    onClick={() => {
+                      val.order > 0 &&
+                        decrease(val);
+                    }}
+                  >
+                    -
+                  </span>
+                </div>
               </span>
             </div>
           ))}
         </div>
       </div>
-    </div>
+    </div >
   );
 }
 
